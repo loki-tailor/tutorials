4bad3bca5a7cc744d2c9e0eae2b1960e6205c740
loki-tailor <lokeshwar.tailor@outlook.com>
2019-12-14 19:25:46 +0530

    updates of 12-Dec-19

--- Test/src/com/modifiers/StrictFpDemo.java
+++ Test/src/com/modifiers/StrictFpDemo.java
@@ -3,6 +3,7 @@ package com.modifiers;
 /**
 		@author lokeshwar created @ 12-12-19
 		Java program to illustrate strictfp modifier
+		TODO: Understand the behavioural difference between using strictfp and not using it.
 */
 
 public class StrictFpDemo {
@@ -13,17 +14,46 @@ public class StrictFpDemo {
 		double num1 = 10e+10;
 		double num2 = 6e+08;
 		double sum = num1 + num2;
-		System.out.println(num1 + " + " + num2 + " = " + sum);
+		System.out.println("strictfp sum: " + num1 + " + " + num2 + " = " + sum);
 		
-	} // end of method
+	} 
 	
 	// calculate large sum without using 
 	public static void normalSum() {
-		
-	} // end of method()
+		double num1 = 10e+10;
+		double num2 = 6e+08;
+		double sum = num1 + num2;		
+		System.out.println("normal sum: " + num1 + " + " + num2 + " = " + sum);
+	} 
+
+
+	// strictfp not allowed on abstract methods	
+	// error: illegal combination of modifiers: abstract and strictfp
+	// strictfp abstract void add(double num1, double num2);
+
+	// strictfp not allowed on variables
+	// error: modifier strictfp not allowed here
+	// strictfp String name = "Lokeshwar";
+
+	// strictfp not allowed on Constructors
+	// error: modifier abstract not allowed here
+	// public abstract StrictFpDemo() {}
 	
 	public static void main(String args[]) {
 		sum();
+		normalSum();
 	} // end of main()
 	
 } // end of class
+
+interface StrictFpInterface {
+	void add();
+
+	// strictfp is not allowed in interface methods or initialized variable (ref below)
+	// compile error: modifier strictfp not allowed here
+	// strictfp int subtract();
+	// strictfp int var2 = -1;
+	
+	// compile time error: = expected
+	// strictfp int var;
+}
---
+++ Test/src/com/modifiers/TransientDemo.java
@@ -0,0 +1,67 @@
+package com.modifiers;
+/**
+	@author = lokeshwar
+	@created = 14-12-19
+	
+	A sample class that uses transient keyword to skip their serialization
+	ref = https://www.geeksforgeeks.org/transient-keyword-java/
+*/
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.util.Date;
+
+public class TransientDemo implements Serializable {
+	
+	// Making password transient for security
+	private transient String password = "default";
+	
+	// Making age transient as age is auto-computable from DOB and current date
+	transient int age;
+
+	// serialize other fields
+	private String username, email;
+	Date dob;
+
+	// normal variables
+	int i = 10, j = 20;
+
+	// transient variable
+	transient int k = 30;
+
+	// user of transient has no impact here
+	transient static int l = 40; 
+	transient final int M = 50;
+
+	public static void main (String[] args) {
+		TransientDemo transientDemo = new TransientDemo();		
+		try {		
+			// serialization
+			FileOutputStream fos = new FileOutputStream("serializedData.txt"); // file created on dir up of com.modifiers
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(transientDemo);
+			System.out.println("i = " + transientDemo.i + " - normal variable");
+			System.out.println("j = " + transientDemo.j + " - normal variable");
+			System.out.println("k = " + transientDemo.k + " - transient int variable");
+			System.out.println("l = " + transientDemo.l + " - transient static int variable");	
+			System.out.println("m = " + transientDemo.M + " - transient final int variable");			
+			System.out.println("Serialization done. Object : " + transientDemo + "\n");			
+
+			// deserialization
+			FileInputStream fis = new FileInputStream("serializedData.txt");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			TransientDemo readObj = (TransientDemo) ois.readObject();
+			System.out.println("i = " + readObj.i + " - normal variable");
+			System.out.println("j = " + readObj.j + " - normal variable");
+			System.out.println("k = " + readObj.k + " - transient int variable");
+			System.out.println("l = " + readObj.l + " - transient static int variable");	
+			System.out.println("m = " + readObj.M + " - transient final int variable");					
+			System.out.println("De-serialization done. Object : " + readObj);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} // end of method
+} // end of class
