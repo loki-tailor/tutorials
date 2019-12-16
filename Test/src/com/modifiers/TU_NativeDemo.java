e6d812132d40e610b835ed0808c88987abba1dee
loki-tailor <lokeshwar.tailor@outlook.com>
2019-12-16 11:36:28 +0530

    native keyword demo

---
+++ Test/src/com/modifiers/TU_NativeDemo.java
@@ -0,0 +1,15 @@
+package com.modifiers;
+
+/**
+	@author = lokeshwar created @ 16-12-19
+	Demonstration of usage of 'native' keyword
+	TODO: Understand JNI - https://stackoverflow.com/a/30635871/7415499
+*/
+
+public class NativeDemo {
+	public native int square (int i);
+	public static void main (String[] args) {
+		System.loadLibrary("Main");
+		System.out.println(new Main().square(2));
+	}
+} // end of class
