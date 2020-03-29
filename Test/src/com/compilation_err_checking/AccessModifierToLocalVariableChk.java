d360e2a05704657243d1a6e83aa7341d2a29775b
loki-tailor <lokeshwar.tailor@outlook.com>
2020-03-29 19:22:22 +0530

    checking local variable & access modifiers

---
+++ Test/src/com/compilation_err_checking/AccessModifierToLocalVariableChk.java
@@ -0,0 +1,20 @@
+package com.compilation_err_checking;
+
+/**-
+	creted @ 29-03-20
+	@author lokeshwar
+*/
+
+public class AccessModifierToLocalVariableChk {
+	void doStuff() {
+
+		// compilation error as : error: illegal start of expression
+		private int x = 7; 
+		public final z = 30;
+
+
+		// no compilation error, as only one [non-access] modifier is allowed i.e. final
+		final int y = 20; 
+		
+	}	
+} // end of class
