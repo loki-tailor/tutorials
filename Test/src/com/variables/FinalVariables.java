95e9c3a83038a3269ba528707635f8b570589217
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-04 22:40:43 +0530

    final var demo

--- Test/src/com/variables/FinalVariables.java
+++ Test/src/com/variables/FinalVariables.java
@@ -3,19 +3,31 @@ package com.variables;
 /*-
 	created @ 31-03-20
 	@author lokeshwar
+	
+	Key Points:
+	* 'final variables cannot be reassigned once assigned a value
+	* 'final' reference variables cannot refer to a different object once the object has been assigned to the final variable
+	* 'final' variable must be initialized before the constructor completes.
+	* There is no such thing as final object. An object reference marked final does NOT mean the object itself can't change. 
 */
 
 public class FinalVariables {
 
-	public FinalVariables() {
-		System.out.println("Inside default constructor");
-		System.out.println("Value of final variable color = " + color);
-	}
+    // below will give compiler error, since
+    // 'final' variables must be initialized before the constructor completes
+    // final String invalid_final_var;
 
-	final String color = "diamond";
+    public FinalVariables() {
+	System.out.println("Inside default constructor");
+	// System.out.println("Value of invalid_final variable color = " +
+	// invalid_final_var);
+	System.out.println("Valid final_variable - " + valid_final_Var);
+    }
 
-	public static void main(String[] args) {
-		FinalVariables fv = new FinalVariables();
-	}
+    final String valid_final_Var = "loki";
+
+    public static void main(String[] args) {
+	FinalVariables fv = new FinalVariables();
+    }
 
 } // end of class
