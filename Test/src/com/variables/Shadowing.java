19b70d82ad3205f0d6e83b1362aa48ba656c2fef
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-04 22:41:36 +0530

    shadowing deo

--- Test/src/com/variables/Shadowing.java
+++ Test/src/com/variables/Shadowing.java
@@ -1,28 +1,38 @@
 package com.variables;
 
-/*-
-	created @ 31-03-20
-	@author lokeshwar
+/*- 	created @ 31-03-20 @author lokeshwar
+	
+	Ref: https://www.dummies.com/programming/java/shadowed-classes-or-variables-in-java/
+	
+	Shadowing refers to the practice in Java programming of using two variables with the same name within scopes that overlap.
 */
 
 public class Shadowing {
-	int count = 9;
+    int count = 9;
 
-	public void logIn() { 
-		int count = 10;
-		System.out.println("local variable count is " + count);
-	}
+    public void logIn() {
+	int count = 10;
+	System.out.println("local variable count is " + count);
+    }
 
-	public void count() { System.out.println("instance variable count is " + count); }
+    public void count() {
+	System.out.println("instance variable count is " + count);
+    }
 
-	public void count(int count) {
-		count = count;
-	}
+    public void count(int count) {
+	count = count;
+    }
 
-	public static void main(String[] args) {
-		Shadowing s = new Shadowing();
-		s.logIn();
-		s.count(20);		
-		s.count();
-	}
+    public static void main(String[] args) {
+	Shadowing s = new Shadowing();
+	s.logIn(); // this prints local variable value
+	s.count(20); // this does nothing, since the assignment has no effect
+	s.count(); // this prints instance variable value
+	
+	// below demonstrates that a variable name can also be same as instance variable name
+	System.out.println();
+	Shadowing Shadowing = new Shadowing();
+	Shadowing.logIn();
+	
+    }
 } // end of class
