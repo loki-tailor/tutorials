a95b7cbb835df3440d080ccdf9f3c792dc35b35b
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-04 22:20:02 +0530

    enum demo

--- Test/src/com/enums/demo2/Device.java
+++ Test/src/com/enums/demo2/Device.java
@@ -2,17 +2,17 @@ package com.enums.demo2;
 
 public class Device {
 
-    Enum1 enum1;
+    Config_API configApi;
+    Api2 api2;
 
     public Device() {
-//	System.out.println("Inside Device() constructor");
-//
-//	X.api2Enum.
-//
-//	configApi.conenct();
-//
-//	api2 = new Api2();
-//	api2.conenct();
+	System.out.println("Inside Device() constructor");
+
+	configApi = new Config_API();
+	configApi.conenct();
+
+	api2 = new Api2();
+	api2.conenct();
     }
 
 }
--- Test/src/com/enums/demo2/Main.java
+++ Test/src/com/enums/demo2/Main.java
@@ -1,13 +1,15 @@
 package com.enums.demo2;
 
+import java.lang.reflect.Field;
+
 public class Main {
 
-    public static void main(String[] args) {
-//	Device d = new Device();
-//	d.configApi.testApi();
-//	d.api2.testApi();
-//	
-//	d.X
+    public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+
+	Device d = new Device();
+
+	Field declaredField = d.getClass().getDeclaredField("configApi");
+
     }
 
 }
