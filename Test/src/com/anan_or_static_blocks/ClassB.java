633a808679c5cb9fc5161a4d7cbacd4c45112571
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-04 23:03:01 +0530

    deleted few files & renamed some

--- Test/src/com/anan_or_static_blocks/ClassB.java
+++
@@ -1,9 +0,0 @@
-package com.anan_or_static_blocks;
-
-public class ClassB {
-
-    public static void main(String[] args) {
-	ClassA classA = ClassA.getInstance();
-    }
-
-}
--- android/Test/app/src/main/java/com/demo/test/Activity2.java
+++ android/Test/app/src/main/java/com/demo/test/Activity2.java
@@ -1,16 +1,21 @@
 package com.demo.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "www.google.com";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
