1b80e9bca13d2a89d8cdf77a6d763d9639ec9572
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-04 22:02:14 +0530

    anon & static blocks

---
+++ Test/src/com/anon_or_static_blocks/ClassA.java
@@ -0,0 +1,21 @@
+package com.anon_or_static_blocks;
+
+public class ClassA {
+
+    {
+	System.out.println("Inside anon block");
+    }
+
+    static {
+	System.out.println("Inside Static block");
+    }
+
+    public ClassA() {
+	System.out.println("Inside Constructor");
+    }
+
+    public static ClassA getInstance() {
+	System.out.println("Inside getInstance()");
+	return new ClassA();
+    }
+}
---
+++ Test/src/com/anon_or_static_blocks/ClassB.java
@@ -0,0 +1,9 @@
+package com.anon_or_static_blocks;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+	ClassA classA = ClassA.getInstance();
+    }
+
+}
