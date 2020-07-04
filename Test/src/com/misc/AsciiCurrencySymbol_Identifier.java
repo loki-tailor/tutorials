9c16126bec8c35deb82189b01ccce516fb548e0a
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-04 22:34:23 +0530

    ascii currency symbol identifiers

---
+++ Test/src/com/misc/AsciiCurrencySymbol_Identifier.java
@@ -0,0 +1,14 @@
+package com.misc;
+
+public class AsciiCurrencySymbol_Identifier {
+
+    public static void main(String[] args) {
+	String ₯lok = "loki";
+	System.out.println(₯lok); // any valid ascii currency symbol can be used
+
+	AsciiCurrencySymbol_Identifier t = new AsciiCurrencySymbol_Identifier();
+	AsciiCurrencySymbol_Identifier Temp = new AsciiCurrencySymbol_Identifier(); // variabe name can be same as class name, it's called 'shadowing
+	
+    }
+
+}
