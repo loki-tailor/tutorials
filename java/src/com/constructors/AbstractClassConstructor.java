adc1f4946046997adff4d356eaa4a1ca84549b79
loki-tailor <lokeshwar.tailor@outlook.com>
2020-07-15 06:28:10 +0530

    abstract class constructor [overloadings]

---
+++ java/src/com/constructors/AbstractClassConstructor.java
@@ -0,0 +1,37 @@
+package com.constructors;
+
+/*- 
+	@author lokeshwar @created 15-07-20
+
+	OUTPUT :- 
+
+	-----------------------------------------------------------
+	Inside abstract class A's - (int) constructor
+	Inside abstract class A's - () constructor
+	Inside overridden method B.dis()
+	Inside abstract class B's - (string) constructor
+	Inside abstract class B's - () constructor
+	-----------------------------------------------------------
+*/
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+abstract class A {
+	A() { this(1); System.out.println("Inside abstract class A's - () constructor"); try { dis(); } catch(Exception e) {} }
+	A(int i) { System.out.println("Inside abstract class A's - (int) constructor"); }
+	
+	abstract protected void dis() throws IOException;
+}
+
+class B extends A {
+	B() { this("1"); System.out.println("Inside abstract class B's - () constructor"); }
+	B(String j) { System.out.println("Inside abstract class B's - (string) constructor"); }
+	public void dis() throws FileNotFoundException { System.out.println("Inside overridden method B.dis()"); }
+}
+
+public class AbstractClassConstructor {
+	public static void main(String[] args) {
+		new B();
+	}
+}
